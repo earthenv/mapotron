fa0683208342fafd732f873c91981a41e8cd8d5a
Jeremy Malczyk <jmalczyk@gmail.com>
2015-04-07 14:56:05 -0400

    A generic assets file

--- ee_assets.py
+++ ee_assets.py
@@ -23,100 +23,27 @@ PALETTES = dict(
 palette = PALETTES["spatial"]
 # Monthly Layers
 layers = OrderedDict()
-
-layers["Evergreen_deciduous_needleleaf_trees_avg"] = {
-   "asset_id" : 'GME/images/04040405428907908306-18223818104584807358',
-   "viz_params" : {"min":0,"max":100,"palette":palette}
+layers["Layer_One"]= {
+    "title" : "The first layer",
+	"asset_id" : "GME/images/some-imageid",
+	"viz_params" : {
+		"min": 0, "max": 2000,
+		"palette": PALETTES["spatial"]
+    }
 }
-layers["Evergreen_broadleaf_trees_avg"] = {
-   "asset_id" : 'GME/images/04040405428907908306-06288355460665902052',
-   "viz_params" : {"min":0,"max":100,"palette":palette}
+layers["Another_Layer"] = {
+    "title" : "Another layer...",
+	"asset_id" : "GME/images/another-imageid",
+	"viz_params" : {
+		"min": 0, "max": 1500,
+		"palette" : PALETTES["bgr"]
+    }
 }
-layers["Deciduous_broadleaf_trees_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-06328252012425307056',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Mixed_other_trees_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-06815775574853865980',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Shrubs_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-00203113007363780479',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Herbaceous_vegetation_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-16835818123240771741',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Cultivated_and_managed_vegetation_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-08243630565672835151',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Regularly_flooded_shrub_herbaceous_vegetation_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-03244650415831943436',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Urban_builtup_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-15614867271382850622',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Snow_ice_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-06438909179914905042',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Barren_lands_sparse_vegetation_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-00046983562667843989',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Open_water_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-17335493681532941844',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Mean_annual_air_temperature_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-14021161906445875414',
-"viz_params" : {"min":0,"max":300,"palette":palette}
-}
-layers["Annual_air_temperature_range_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-17420260840459206438',
-"viz_params" : {"min":76,"max":500,"palette":palette}
-}
-layers["Annual_sum_of_precipitation_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-03748644437782705176',
-"viz_params" : {"min":0,"max":20000000,"palette":palette}
-}
-layers["Annual_precipitation_avg"] = {
-"asset_id" : 'GME/images/04040405428907908306-05408005842133179591',
-"viz_params" : {"min":0,"max":100,"palette":palette}
-}
-layers["Catchment_size_sum"] = {
-"asset_id" : 'GME/images/04040405428907908306-14916877419814061790',
-"viz_params" : {"min":1,"max":5000,"palette":palette}
-}
-layers["Stream_length_sum"] = {
-"asset_id" :  'GME/images/04040405428907908306-18258810609658841024',
-"viz_params" : {"min":1,"max":500,"palette":palette}
-}
-layers["Elevation_avg"] = {
-"asset_id" :  'GME/images/04040405428907908306-14725983039031760735',
-"viz_params" : {"min":0,"max":3000,"palette":palette}
-}
-layers["Elevation_range"] = {
-"asset_id" :  'GME/images/04040405428907908306-01331975681272697730',
-"viz_params" : {"min":0,"max":3000,"palette":palette}
-}
-layers["Slope_avg"] = {
-"asset_id" :  'GME/images/04040405428907908306-15738620217066490177',
-"viz_params" : {"min":0,"max":500,"palette":palette}
-}
-layers["Slope_range "] = {
-"asset_id" :  'GME/images/04040405428907908306-18241744787223608951',
-"viz_params" : {"min":0,"max":500,"palette":palette}
-}
-layers["Precambrian_surface_lithology_wsum"] = {
-"asset_id" :  'GME/images/04040405428907908306-17269292502416777314',
-"viz_params" : {"min":0,"max":40,"palette":palette}
-}
-layers["Quaternary_surface_lithology_wsum"] = {
-"asset_id" :  'GME/images/04040405428907908306-17590838206930784687',
-"viz_params" : {"min":0,"max":40,"palette":palette}
+layers["You_get_the_point"] = {
+    "title" : "You get the point",
+	"asset_id" : "GME/images/yetonemore-imageid",
+	"viz_params" : {
+		"min": 0, "max": 2500,
+		"palette": PALETTES["blue"]
+    }
 }
