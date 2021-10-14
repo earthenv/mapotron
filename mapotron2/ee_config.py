27a6f6788787524568c7463424ad82f0495e833a
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2021-10-13 22:40:34 -0400

    Use local config for layers (temporarily?)

--- mapotron2/ee_config.py
+++ mapotron2/ee_config.py
@@ -2,8 +2,12 @@ import json
 import requests
 import ee
 
-config = json.loads(requests.get(
-    'http://earthenv.org/map_config/config.json').content)
+# config = json.loads(requests.get(
+#     'http://earthenv.org/map_config/config.json').content)
+
+# Use local config.json
+with open('static/config.json') as cf:
+    config = json.load(cf)
 
 collections = {}
 for collection in config:
@@ -12,13 +16,11 @@ for collection in config:
     for feature in collection["features"]:
         collections[collection["id"]]["layers"][feature["layer_id"]] = feature
 
-ee.Initialize()
+# ee.Initialize()
 
-# import ee
+GEE_SERVICE_ACCOUNT = "map-of-life@appspot.gserviceaccount.com"
+GEE_SERVICE_KEY = "keys/map-of-life-6095c82f9146.json"
 
-# GEE_SERVICE_ACCOUNT = "ACC"
-# GEE_SERVICE_KEY = "PATH"
+EE_CREDENTIALS = ee.ServiceAccountCredentials(GEE_SERVICE_ACCOUNT, GEE_SERVICE_KEY)
 
-# EE_CREDENTIALS = ee.ServiceAccountCredentials(GEE_SERVICE_ACCOUNT, GEE_SERVICE_KEY)
-
-# ee.Initialize(credentials=EE_CREDENTIALS)
+ee.Initialize(credentials=EE_CREDENTIALS)
