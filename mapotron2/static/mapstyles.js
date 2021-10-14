e7c1b91243fb02ab6c8667ea1c53577666f8d5a4
Ajay Ranipeta <ajay.ranipeta@gmail.com>
2021-10-13 22:50:37 -0400

    Add dark map style

--- mapotron2/static/mapstyles.js
+++ mapotron2/static/mapstyles.js
@@ -48,5 +48,45 @@ styles = [
     }
 ];
 
+styles_dark = [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+        featureType: "administrative",
+        stylers: [{ "visibility": "on" }],
+      },
+      {
+        featureType: "administrative.country",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563", "visibility": "on" }],
+      },
+        {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563", visibility: "off" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e", visibility: "off" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+  ];
+
 //var basicMapType = new google.maps.StyledMapType(styles,
 //    {name: "Basic"});
